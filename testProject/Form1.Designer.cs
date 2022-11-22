d8d97989f3da751ac49e819552e79a41f83bd868
pellmell1 <good@sigpact.com>
2022-11-22 20:53:27 +0900

    초기 커밋.

---
+++ testProject/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace testProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Button = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "결과";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(336, 71);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(115, 29);
+            this.Button.TabIndex = 1;
+            this.Button.Text = "양불판단";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(265, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "밝기";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(396, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "균일도";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(527, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "CR비";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox5.Font = new System.Drawing.Font("맑은 고딕", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5.Location = new System.Drawing.Point(100, 198);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(159, 114);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("맑은 고딕", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox7.Location = new System.Drawing.Point(265, 198);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(125, 114);
+            this.textBox7.TabIndex = 2;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Font = new System.Drawing.Font("맑은 고딕", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox10.Location = new System.Drawing.Point(396, 198);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(125, 114);
+            this.textBox10.TabIndex = 3;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Font = new System.Drawing.Font("맑은 고딕", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox14.Location = new System.Drawing.Point(527, 198);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(125, 114);
+            this.textBox14.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox14;
+    }
+}
+
---
+++ testProject/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace testProject
+{
+
+
+
+	public partial class Form1 : Form
+	{
+
+
+		Microsoft.Office.Interop.Excel.Application app = null;
+		Workbook wb = null;
+		Worksheet ws = null;
+
+		enum Column { NO,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,AA,AB,AC,AD,AE,AF }		
+
+		public Form1()
+		{
+			InitializeComponent();
+			
+		}
+
+
+		private void Button_Click(object sender, EventArgs e)
+		{
+		
+
+			try
+			{
+                
+                
+                app = new Microsoft.Office.Interop.Excel.Application();
+				app.Visible = true;
+	
+				wb = app.Workbooks.Open(Filename : @"C:\projector\illuminance meter.xlsx");
+				ws = wb.Worksheets.get_Item(1) as Worksheet;
+				Microsoft.Office.Interop.Excel.Range range = ws.UsedRange;
+
+				for (int row = 1; row <= range.Rows.Count; row++) // 가져온 행 만큼 반복
+				{
+					for (int column = 1; column <= range.Columns.Count; column++)  // 가져온 열 만큼 반복
+					{
+						string str = (string)(range.Cells[row, column] as Microsoft.Office.Interop.Excel.Range).Value2;  // 셀 데이터 가져옴
+
+						Console.Write(str + " ");
+					}
+					Console.WriteLine();
+				}
+				//Microsoft.Office.Interop.Excel.Range Area = ws.Cells[Column.G, 2];
+				//Microsoft.Office.Interop.Excel.Range G5 = ws.Cells[Column.G, 5];
+
+				// nsole.WriteLine("test--" + Area.Value);
+
+			}
+			catch (Exception exc)
+			{
+				MessageBox.Show(exc.Message);
+			}
+			finally
+			{
+				//변경점 저장하면서 닫기
+				wb.Close(true);
+				//Excel 프로그램 종료
+				app.Quit();
+				//오브젝트 해제
+				ReleaseExcelObject(wb);
+				ReleaseExcelObject(app);
+			}
+		}
+
+		private void ReleaseExcelObject(object obj)
+		{
+			try
+			{
+				if (obj != null)
+				{
+					Marshal.ReleaseComObject(obj);
+					obj = null;
+				}
+			}
+			catch (Exception ex)
+			{
+				obj = null;
+				throw ex;
+			}
+			finally
+			{
+				GC.Collect();
+			}
+		}
+
+    }
+
+
+
+}
+
+
+
